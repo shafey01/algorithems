696387fafe3195e1603439d5f5ee59b1409abf1c
shafey01 <shafey0101@gmail.com>
2025-01-01 04:46:10 +0200

    Recrusion

--- Graphs/main.go
+++ Graphs/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
-	"github.com/shafey01/algorithems/Graphs/graph"
+	"fmt"
+
+	_ "github.com/shafey01/algorithems/Graphs/graph"
 )
 
 func main() {
-	graph := graph.NewGraph()
+	// graph := graph.NewGraph()
+	//
+	//	for i := 0; i < 5; i++ {
+	//		graph.AddVertex(i)
+	//	}
+	//
+	// graph.AddEdge(1, 2)
+	// graph.AddEdge(1, 2)
+	// graph.AddEdge(6, 2)
+	// graph.AddEdge(3, 2)
+	// graph.AddEdge(1, 4)
+	// graph.AddEdge(1, 5)
+	//
+	// graph.Print()
+	fmt.Println(nill(nil, &Node{}))
+}
 
-	for i := 0; i < 5; i++ {
-		graph.AddVertex(i)
+type Node struct {
+	val   int
+	left  *Node
+	right *Node
+}
+
+func nill(l *Node, r *Node) bool {
+	if l == nil || r == nil {
+		fmt.Println("if nil: ")
+		return l == r
 	}
 
-	graph.AddEdge(1, 2)
-	graph.AddEdge(1, 2)
-	graph.AddEdge(6, 2)
-	graph.AddEdge(3, 2)
-	graph.AddEdge(1, 4)
-	graph.AddEdge(1, 5)
-
-	graph.Print()
+	return true
 }
